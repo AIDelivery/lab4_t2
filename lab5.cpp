c2e801161a2505e46de057ad3a653903009a1d64
Dan <daniyar.burakaev@mail.ru>
2019-10-30 17:53:03 +0300

    Ready!

--- lab5.cpp
+++ lab5.cpp
@@ -1,33 +1,60 @@
 #include <pthread.h>
 #include <string.h>
 
+#include <fstream>
 #include <iostream>
 using namespace std;
 
-void* div_by_thread(void* p) {
+ifstream if1;
+ofstream of1, of2;
 
+void* even_string(void* v) {
+  // cout << "[EVEN THREAD] " << (char*) v << endl;
+  cout << (char*) v << endl;
+  of1 << (char*) v << endl;
+
+  pthread_exit(EXIT_SUCCESS);
 }
+
+void* odd_string(void* v) {
+  // cout << "[ODD THREAD] " << (char*) v << endl;
+  cout << (char*) v << endl;
+  of2 << (char*) v << endl;
+
+  pthread_exit(EXIT_SUCCESS);
+}
+
 
 int main(int argv, char* argc[]) {
   pthread_t thread1, thread2;
-  FILE *fi, *fo1, *fo2;
   pthread_attr_t attr;
+  string line, *l_p = &line;
 
-  string* cur_str = new string();
+  if1.open("INFO.txt");
+  of1.open("LOG1.txt");
+  of2.open("LOG2.txt");
 
-  fi = fopen("INFO.txt", "r");
-  // fo1 = fopen("LOG1.txt", "w");
-  // fo2 = fopen("LOG2.txt", "w");
-
-  if(!fi || !fo1 || !fo2) {
-    cout << "ERROR";
-    return 0;
+  if(if1.is_open() && of1.is_open() && of1.is_open())
+    cout << "File openning... Success" << endl;
+  else {
+      cout << "ERROR" << endl;
+      return 0;
   }
 
   pthread_attr_init(&attr);
-  pthread_create(&thread1, &attr, div_by_thread, (void*) cur_str);
 
-  char c;
-  while((c = fgetc(fi)) != EOF)
-    cout << c;
+  for(int i = 0; getline(if1, line); i++) {
+    if(i%2 == 0) {
+        pthread_create(&thread1, &attr, even_string, (void*) line.c_str());
+        pthread_join(thread1, NULL);
+    }
+    else {
+      pthread_create(&thread2, &attr, odd_string, (void*) line.c_str());
+      pthread_join(thread2, NULL);
+    }
+  }
+
+  if1.close();
+  of1.close();
+  of2.close();
 }
